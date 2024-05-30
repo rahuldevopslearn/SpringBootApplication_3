14b9d149f3b7dbd903088d5918851b980881742c
Rahul <knrn23@gmail.com>
2024-05-30 16:44:16 +0530

    first commit

---
+++ src/main/java/com/star/agile/assignment/springboot_demo_3/SpringbootDemo3Application.java
@@ -0,0 +1,13 @@
+package com.star.agile.assignment.springboot_demo_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemo3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/star/agile/assignment/springboot_demo_3/SpringbootDemo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.star.agile.assignment.springboot_demo_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
